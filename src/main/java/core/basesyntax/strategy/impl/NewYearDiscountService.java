1a28b27c7755f0d752b7039ebd6a4a378eebc914
Anastasiia <nastyagoncharenko5@gmail.com>
2024-07-13 02:54:37 +0300

    WORK-9 creating discount strategy implementation

---
+++ src/main/java/core/basesyntax/strategy/impl/NewYearDiscountService.java
@@ -0,0 +1,13 @@
+package core.basesyntax.strategy.impl;
+
+import core.basesyntax.strategy.DiscountService;
+
+public class NewYearDiscountService implements DiscountService {
+
+    private static final double DISCOUNT = 20;
+
+    @Override
+    public double getDiscount() {
+        return DISCOUNT;
+    }
+}
